9ae0de22fa3281e839d08728f4f16494479f5168
doyoung <doyoh1179@gmail.com>
2022-04-19 23:27:45 +0900

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas culpa voluptates unde nemo in atque molestiae ratione sunt, distinctio odit iure omnis minus illo non nesciunt aliquam, magni, aspernatur natus quasi possimus repudiandae deserunt? Blanditiis vel nulla deleniti? Reiciendis, suscipit!
+      </p>
+    </section>
+  )
+}
+
+export default About
